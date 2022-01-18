9015561fa0815f7144478038ff4d57c3e6fbe53e
qkx <18438605721@163.com>
2022-01-18 19:14:59 +0800

    Update countvec.py

--- countvec.py
+++ countvec.py
@@ -1,4 +1,10 @@
 from sklearn.feature_extraction.text import CountVectorizer
+
+df['RECRUIT_ID'] = df['RECRUIT_ID'].astype(str)
+df['PERSON_ID'] = df['PERSON_ID'].astype(str)
+
+data = df.groupby('RECRUIT_ID')['PERSON_ID'].apply(list).reset_index()
+
 cv = CountVectorizer()
 cv_fit = cv.fit_transform(data['PERSON_ID'].astype(str))
 #获取单词列表
